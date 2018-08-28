2123779960c4bdd9b8dbbbc5cd449813ba61aa44
Marco Meneghini <marco_meneghini@hotmail.com>
2018-08-28 16:29:56 +0200

    Add project files.

---
+++ BakeUpBackendR1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BakeUpBackendR1.MvcApplication" Language="C#" %>
---
+++ BakeUpBackendR1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BakeUpBackendR1.Startup))]
+namespace BakeUpBackendR1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
